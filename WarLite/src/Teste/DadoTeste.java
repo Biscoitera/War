43fdd5dea820d7f26c0243d4a293eae9f41813f0
Biscoitera <mudo154@hotmail.com>
2018-06-14 17:05:26 -0300

    More JUnit
    Tentativa de fazer o JUnit reconhecer as classes

--- WarLite/src/Teste/DadoTeste.java
+++ WarLite/src/Teste/DadoTeste.java
@@ -2,13 +2,19 @@ package Teste;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class DadoTeste {
+	Dado d = new Dado(6);
+	@Test
+	void testRoll() {
+		Assertions.assertFalse(d.roll(0) > 6);
+	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testDado() {
+		Assertions.assertEquals(6, d.getMax());
 	}
 
 }
